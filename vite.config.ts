b0f24fcc0b50ca868d4ab7f293eee52f67438f14
code-pa-32 <vernichenko.aleksandr@gmail.com>
2026-07-10 01:08:23 +0300

    feat: app version

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
-import pkg from './package.json' with { type: 'json' };
+import pkg from './package.json' with { type: 'json' }
 
-process.env.VITE_APP_VERSION = pkg.version;
+process.env.VITE_APP_VERSION = pkg.version
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
